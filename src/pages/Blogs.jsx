779266163049f459958cc4867b3e3fb3f1fee143
Himanshu Tiwari <himanshu.20220026@mnnit.ac.in>
2025-07-07 17:59:58 +0530

    Blog Data Page added

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { assets, blog_data, comments_data } from '../assets/assets';
+import Navbar from '../components/Navbar';
+import Moment from 'moment';
+import Footer from '../components/Footer';
+import Loader from '../components/Loader';
 
 const Blogs = () => {
-  return (
-    <div>Blogs</div>
-  )
+  const {id} = useParams();
+  const [data,setData] = useState(null);
+  const [comments,setComments] = useState([]);
+
+  const [name,setName] = useState('');
+  const [content,setContent] = useState('');
+  
+
+  const addComment = async(e)=>{
+    e.preventDefault();
+    if(!name.trim() || !content.trim()) return;
+
+    const newComment = {
+      name,
+      content,
+      createdAt:new Date().toISOString(),
+    }
+
+    setComments([newComment, ...comments]);
+    setName('');
+    setContent('');
+  }
+
+  const fetchComments = async()=>{
+    setComments(
+      comments_data
+    )
+  }
+
+  const fetchBlogData = async()=>{
+    try{
+      const data =  blog_data.find(item => item._id  === id );
+      setData(data);
+    }
+    catch(error){
+      console.error("Error fetching blog data:",error);
+    }
+  }
+  useEffect(()=>{
+     fetchBlogData();
+     fetchComments();
+  },[])
+
+  return  data ? (
+    
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt="" className='absolute inset-0 w-full h-full object-cover -z-11 opacity-50' />
+      <Navbar></Navbar>
+      <div className='text-center mt-20 text-gray-600'>
+         <p className='text-primary py-4 font-medium'>Publised on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+         <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
+         <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+         <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-purple-500 font-medium text-white'>Himanshu Tiwari</p>
+       </div>
+
+       <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
+        <img src={data.image} alt=""  className='rounded-3xl mb-5'/>
+        <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html : data.description}}>
+
+        </div>
+        {/* Comments Section */}
+        <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+            <p className='font-semibold mb-4 text-xl text-underline'>Comments ({comments.length}):</p>
+            <div className='flex flex-col gap-4'>
+                {comments.map((item,index)=>(
+                    <div key={index} className='relative bg-gray-700 border border-gray-300 max-w-xl p-4 rounded text-gray-600'>
+                      <div className='flex items-center gap-2 mb-2'>
+                        <img src={assets.user_icon} alt="" className='w-6 text-white' />
+                        <p className='font-medium text-white'>{item.name}</p>
+                      </div>
+                      <p className='text-sm max-w-md ml-8 text-white'>{item.content}</p>
+                      <div className='absolute right-4 bottom-3 flex items-center gap-2 text-x5 text-white'>{Moment(item.createdAt).fromNow()}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+          {/* Add comment section */}
+          <div onSubmit={addComment} className='max-w-3xl mx-auto '>
+            <p className='font-semibold mb-4 text-xl'>Add your Comment :</p>
+            <form  className='flex flex-col items-start gap-4 max-w-lg'>
+              <input onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 rounded '/>
+              <textarea onChange={(e)=>setContent(e.target.value)} value={content} placeholder='Type your Comment...' className='w-full p-2 border border-gray-300 rounded  h-50'></textarea>
+              <button type='submit' className='border border-gray-300 rounded-md px-5 py-2 bg-purple-500 cursor-pointer hover:scale-105'>Submit</button>
+            </form>
+            
+          </div>
+       </div>
+      <Footer/>
+    </div>
+  ) : <Loader/>
 }
 
 export default Blogs
